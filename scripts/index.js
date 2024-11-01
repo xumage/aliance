eca1daf1c818394f39381941c11f097ca19423d2
Maris <msarafutdinov0@gmail.com>
2024-11-01 20:27:29 +0500

    mobile header menu modified

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,11 @@ burgerButton.addEventListener("click", function () {
     // Переключаем класс active у мобильного меню
     header.classList.toggle("opened");
 });
+
+const dropDowns = document.getElementsByClassName("dropdownSelected");
+for (let i = 0; i < dropDowns.length; i++) {
+    dropDowns[i].addEventListener("click", function () {
+        // Переключаем класс "selected" у текущего элемента
+        this.classList.toggle("selected");
+    });
+}
