a2b29337212976b2d1b364a38c89eba972f64d1b
Maris <msarafutdinov0@gmail.com>
2024-11-03 20:35:40 +0500

    hero section

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,29 @@
 const burgerButton = document.getElementById("burgerButton");
 const mobileMenu = document.getElementById("mobileMenu");
 
-// Добавляем обработчик события на бургер-кнопку
 burgerButton.addEventListener("click", function () {
-    // Переключаем класс active у мобильного меню
     header.classList.toggle("opened");
 });
 
 const dropDowns = document.getElementsByClassName("dropdownSelected");
 for (let i = 0; i < dropDowns.length; i++) {
     dropDowns[i].addEventListener("click", function () {
-        // Переключаем класс "selected" у текущего элемента
         this.classList.toggle("selected");
     });
 }
+
+new Swiper(".swiper", {
+    slidesPerView: 4,
+    breakpoints: {
+        1: { slidesPerView: 1 },
+        400: { slidesPerView: 2 },
+        600: { slidesPerView: 3 },
+        800: { slidesPerView: 4 },
+        1024: { slidesPerView: 5 },
+    },
+    // 1280: { slidesPerView: 5 },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
